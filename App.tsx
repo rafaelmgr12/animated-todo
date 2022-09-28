8bcf2007b470d89d875c9a1a802ba468ebdf5b0d
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-09-28 20:35:54 -0300

    feat: add component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppContainer from './src/components/app-container';
+
 
 export default function App() {
   return (
+    <AppContainer>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text> Hello World!!!!!</Text>
       <StatusBar style="auto" />
     </View>
+    </AppContainer>
   );
 }
 
